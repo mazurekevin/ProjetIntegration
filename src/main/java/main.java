29e268fb5690f842802afe4c85465a47ea675aac
kevin <kevin.mazure.km@gmail.com>
2022-03-06 20:37:40 +0100

    push erreur

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,6 +4,6 @@ import com.Interface.ContentTask;
 import javax.swing.*;
 public class main {
     public static void main(String[] args) {
-        JFrame frame = new Accueil();
+        JFrame frame = new Acceil();
     }
 }
